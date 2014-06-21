aca38de331e1b3db365aa335ca0c34cacbceee44
Adrien RUIZ <adrien.ruiz083@gmail.com>
2014-06-21 11:20:29 +0200

    Contrôle sur le joueur
    Ajout du contrôle sur le joueur.
    Click gauche: rotation vers la gauche.
    Click droit: rotation vers la droite.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private bool _rotateLeft;
+	private bool _rotateRight;
+	private int torque;
+
+	void Start () {
+		rotateLeft = false;
+		rotateRight = false;
+	} // Start ();
+
+	public bool rotateLeft {
+		get {return _rotateLeft;}
+		set {_rotateLeft = value;}
+	} // rotateLeft;
+
+	public bool rotateRight {
+		get {return _rotateRight;}
+		set {_rotateRight = value;}
+	} // rotateRight;
+
+	void Update () {
+		torque = rotateRight ? -1 : rotateLeft ? 1 : 0;
+	} // Update ();
+
+	public void FixedUpdate () {
+		rigidbody2D.AddTorque (torque);
+	} // FixedUpdate ();
+
+}; // Player;
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	
+	private Player player;
+	private bool mouseLeftDown;
+	private bool mouseRightDown;
+
+	public void Start () {
+		player = GetComponent<Player> ();
+		mouseLeftDown = false;
+		mouseRightDown = false;
+	} // Start ();
+
+	public void Update () {
+		// Rotation vers la gauche ?
+		if (Input.GetMouseButtonDown (0)) {
+			mouseLeftDown = true;
+		} else if (Input.GetMouseButtonUp (0)) {
+			mouseLeftDown = false;
+		}
+		player.rotateLeft = mouseLeftDown;
+
+		// Rotation vers la droite ?
+		if (Input.GetMouseButtonDown (1)) {
+			mouseRightDown = true;
+		} else if (Input.GetMouseButtonUp (1)) {
+			mouseRightDown = false;
+		}
+		player.rotateRight = mouseRightDown;
+	} // Update ();
+
+}; // PlayerControl;
